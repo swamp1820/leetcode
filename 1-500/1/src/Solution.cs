0ba7b519f507ddda968745864e3d3c7101e78bba
swamp1820 <swamp1820@yandex.ru>
2023-03-12 21:44:40 +0300

    1st

---
+++ 1-500/1/src/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace src;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var result = new int[] { };
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                };
+            };
+        };
+
+        return result;
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # LeetCode
 
+- 1 [Two Sum](https://leetcode.com/problems/two-sum/)
 - 14 [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/)
 - 21 [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/)
 - 28 [Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/)
